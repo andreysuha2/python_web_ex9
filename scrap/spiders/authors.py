c9ffc5fc24075b297c6b5fa877331a70da1987a8
Andrii Sukhenko <andrii.suhenko@gmail.com>
2024-01-25 15:22:49 +0200

    add authors parser

--- scrap/spiders/authors.py
+++ scrap/spiders/authors.py
@@ -5,11 +5,17 @@ import scrapy
 class AuthorsSpider(scrapy.Spider):
     name = "authors"
     allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ["https://quotes.toscrape.com"]
+    custom_settings = {"FEED_FORMAT": "json", "FEED_URI": "authors.json"}
     
     def start_requests(self):
-        print("Authors urls", authors_urls.urls)
-        return super().start_requests()
+        for url in authors_urls.urls:
+            yield scrapy.Request(f"https://quotes.toscrape.com{url}/", callback=self.parse)
 
     def parse(self, response):
-        yield {}
+        author = response.xpath("/html//div[@class='author-details']")
+        yield {
+            "fullname": author.xpath("h3[@class='author-title']/text()").get(),
+            "born_date": author.xpath("//span[@class='author-born-date']/text()").get(),
+            "born_location": author.xpath("//span[@class='author-born-location']/text()").get(),
+            "description": author.xpath("div[@class='author-description']/text()").get()
+        }
